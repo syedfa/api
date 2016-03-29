581e7d8de641aad53bd6aa40c061d53a40c95cb1
Asep Bagja Priandana <asep@asep.co>
2016-03-29 12:00:43 +0700

    changing the git command in bash script

--- _bash_scripts/deploy.sh
+++ _bash_scripts/deploy.sh
@@ -5,11 +5,11 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   cd _site
   git init
                       
-    git remote add deploy "deploy@128.199.215.215:/home/deploy/node_app/halal_data_api/app"
+  git remote add deploy "deploy@128.199.215.215:/home/deploy/node_app/halal_data_api/app"
   git config user.name "Travis CI"
   git config user.email "asep@asep.co"
                               
-  git add .
+  git add --all .
   git commit -m "Deploy"
   git push --force deploy master
 else
