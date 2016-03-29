d5c3c920e9ba564251ecbd2593a2bfbf12e8ffa4
Asep Bagja Priandana <asep@asep.co>
2016-03-29 12:50:01 +0700

    Changing the hello message in app.js. Only want to check the post-receive hook.

--- _bash_scripts/deploy.sh
+++ _bash_scripts/deploy.sh
@@ -11,7 +11,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
                               
   git add --all .
   git commit -m "Deploy"
-  git push --force deploy master
+  git push --force deploy master 
 else
   echo "Not deploying, since this branch isn't master."
 fi
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *() {
-  this.body = "Hello, Halal Data";
+  this.body = "Halal Data API v1.";
 });
 
 app.listen(3000);
